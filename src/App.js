bf19344602e48c1d2686cba49892a137f76df24f
SunF0x <buzinakatya2000@mail.ru>
2023-05-09 23:59:45 +0300

    Start react project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-neutral-100">
+      <div className="rectangle">
+        <h5 className="px-8 py-8 text-2xl text-white">Word Keeper</h5>
+      </div>
+    </div>
+  );
+}
+
+export default App;
