d0c49aae2b62fd232838de51da27fb9e991d57ad
SunF0x <buzinakatya2000@mail.ru>
2023-05-10 21:37:45 +0300

    Added precommit hook

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
 /** @type {import('tailwindcss').Config} */
+/* eslint-env node */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
-}
-
+  plugins: []
+};
